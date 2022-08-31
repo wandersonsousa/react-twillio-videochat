4d165ff566b44aeaaa5288e4a3aa83b6ac0437db
Wanderson <wandersonsousa3004@gmail.com>
2022-08-31 11:06:42 -0300

    chore: add nextconfig

---
+++ next.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  reactStrictMode: false,
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    domains: ["firebasestorage.googleapis.com"],
+  },
+};
